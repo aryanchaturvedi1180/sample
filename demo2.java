487304bde1a002d02d17fd8f6d0855c6abdf06fd
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2026-01-13 22:53:48 +0530

    up to date

--- demo2.java
+++
@@ -1,6 +0,0 @@
-public class demo2 {
-    public static void main(String[] args) {
-        System.out.println("Hello from demo2!... How are you Buddy....");
-    }
-    
-}
--- demo3.java
+++
@@ -1,6 +0,0 @@
-public class demo3 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello from demo3!... What's up Friend.... from code 2 branch");
-    }
-}
