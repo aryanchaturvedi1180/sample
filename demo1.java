a0b5850a693af199be8df601f50503b258fdb8cc
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2026-01-13 21:47:48 +0530

    [Aryan] Add.from branch code 2 commiting demo1.java

--- demo1.java
+++ demo1.java
@@ -1,7 +1,5 @@
 public class demo1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Bhai.. from code 2 branch!");
     }
-    
-    
 }
