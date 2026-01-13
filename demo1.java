9c3c28db3ed4de531da3d06042be8653a2ed928b
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2026-01-13 22:15:49 +0530

    code1 changed demo1

--- demo1.java
+++ demo1.java
@@ -1,6 +1,7 @@
 public class demo1 {
     public static void main(String[] args) {
-        System.out.println("Hello, Bhai.. from code 2 branch!");
+        System.out.println("Hello from CODE 1 branch");
+
     }
     
     
